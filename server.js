01bbfbe3dc7ff4c850680af3e9c1ae7a9e8251e4
Jonathan <eimerarts@gmail.com>
2020-08-09 13:42:28 -0500

    retry

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+const fs = require('fs');
 const express = require('express');
+const PORT = process.env.PORT || 3001;
 const app = express();
 const path = require('path');
 
 
-const notes = require('./Develop/db/db.json')
-const PORT = 3001;
+const notes = require('./db/db.json')
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -24,15 +26,25 @@ app.get('/api/notes', (req, res) => {
 
 app.post('/api/notes', (req, res) => {
   const newNotes = req.body;
+  newNotes.id = notes.length + 1;
   console.log(newNotes);
   console.log(notes);
   notes.push(newNotes);
+
+  fs.writeFile('./db/db.json', JSON.stringify(notes), function (err) {
+    if (err) throw err;
+    console.log('Saved!');
+  });
+
   res.json(newNotes);
-  
 });
 
 
 
+
+
+
+
 app.listen(PORT, () => {
-    console.log(`App listening on PORT ${PORT}`);
-  });
+  console.log(`API server now on port ${PORT}!`);
+});
